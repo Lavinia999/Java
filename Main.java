1502661d7b92ce714f7ad193ba5d59c6815d8cdf
Lavinia999 <78488564+Lavinia999@users.noreply.github.com>
2021-04-01 13:16:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Lavinia!");
+        System.out.println("Cats are robots.");
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println("WE NEED TO");
+        System.out.println("LEARN JAVA");
+        System.out.println("AS QUICKLY AS POSSIBLE");
+        System.out.println("O X X");
+        System.out.println("O X O");
+        System.out.println("X O X");
+    }
+}
